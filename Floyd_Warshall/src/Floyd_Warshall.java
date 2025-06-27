ae74d23a14e1d791ab111a382e1de23b4327b751
OblivionSword <hunter140797@gmail.com>
2025-06-27 16:54:10 +0200

    make the floyd-warshall logic clearer to read

--- Floyd_Warshall/src/Floyd_Warshall.java
+++ Floyd_Warshall/src/Floyd_Warshall.java
@@ -6,19 +6,18 @@ public class Floyd_Warshall {
         int V = graph.length;
         int[][] dist = new int[V][V];
 
-        // Copy graph into dist matrix
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
                 dist[i][j] = graph[i][j];
             }
         }
 
-        // Main Floyd-Warshall logic with overflow protection
+        // Floyd-Warshall
         for (int k = 0; k < V; k++) {
             for (int i = 0; i < V; i++) {
                 for (int j = 0; j < V; j++) {
                     if (dist[i][k] != INF && dist[k][j] != INF &&
-                        dist[i][k] + dist[k][j] < dist[i][j]) {
+                    	dist[i][j] > dist[i][k] + dist[k][j]) {
                         dist[i][j] = dist[i][k] + dist[k][j];
                     }
                 }
