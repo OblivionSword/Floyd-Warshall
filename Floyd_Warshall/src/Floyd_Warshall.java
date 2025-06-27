6f4245ee37af35aa4c8e4a29bc6390fa6d3cbc0d
OblivionSword <hunter140797@gmail.com>
2025-06-27 17:04:36 +0200

    rewrite the Floyd_Warshall according to the Pseudocode from Wikipedia

--- Floyd_Warshall/src/Floyd_Warshall.java
+++ Floyd_Warshall/src/Floyd_Warshall.java
@@ -3,13 +3,24 @@ public class Floyd_Warshall {
 	static final int INF = Integer.MAX_VALUE;
 
     public int[][] floydWarshall(int[][] graph) {
-        int V = graph.length;
+    	int V = graph.length;
         int[][] dist = new int[V][V];
 
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < V; j++) {
-                dist[i][j] = graph[i][j];
+        // let dist be a |V| × |V| array of minimum distances initialized to ∞ (infinity)
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                dist[i][j] = INF;
+
+        for (int u = 0; u < V; u++) {
+            for (int v = 0; v < V; v++) {
+                if (graph[u][v] != INF) {
+                    dist[u][v] = graph[u][v];
+                }
             }
+        }
+
+        for (int v = 0; v < V; v++) {
+            dist[v][v] = 0;
         }
 
         // Floyd-Warshall
@@ -17,7 +28,7 @@ public class Floyd_Warshall {
             for (int i = 0; i < V; i++) {
                 for (int j = 0; j < V; j++) {
                     if (dist[i][k] != INF && dist[k][j] != INF &&
-                    	dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] > dist[i][k] + dist[k][j]) {
                         dist[i][j] = dist[i][k] + dist[k][j];
                     }
                 }
